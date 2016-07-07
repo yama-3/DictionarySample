0ac8d288795b53453e2f62c9f326ec6a0ea39896
Kazuhiro Yamabe <kazuhiro.yamabe@gmail.com>
2016-07-07 23:40:01 +0900

    search word in dictionary which bundled iOS

--- DictionarySample/ViewController.swift
+++ DictionarySample/ViewController.swift
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBAction func search(sender: UIButton) {
+        if let text = searchTextField.text {
+            self.search(text)
+        }
+    }
+    
+    @nonobjc
+    func search(term: String) {
+        if UIReferenceLibraryViewController.dictionaryHasDefinitionForTerm(term) {
+            let vc = UIReferenceLibraryViewController(term: term)
+            presentViewController(vc, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "not found", message: "not found definitions", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+            presentViewController(alert, animated: true, completion: nil)
+        }
+    }
 
 }
 
