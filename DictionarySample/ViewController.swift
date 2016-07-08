b4fad0836e4587245d451ffbf0b46a6900dcff91
yama3 <kazuhiro.yamabe@gmail.com>
2016-07-08 23:06:05 +0900

    add Pods (ChameleonFramework)

--- DictionarySample/ViewController.swift
+++ DictionarySample/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.tintColor = UIColor.flatGreenColorDark()
+        //self.view.backgroundColor = UIColor.flatMintColor()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'DictionarySample' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DictionarySample
+  pod 'ChameleonFramework/Swift'
+end
